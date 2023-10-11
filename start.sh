dd3a595cf659c1a7113836a9e9405775f5c32b3e
ohainz <124523037+ohainz@users.noreply.github.com>
2023-10-11 09:55:22 +0200

    Add output to start script

--- start.sh
+++ start.sh
@@ -18,20 +18,34 @@ then
     VNCPassword="test"
 fi
 
+echo "Restart lighttpd"
 systemctl restart lighttpd
 
+echo "Create user $UserName"
 # useradd test -p test -m
 useradd $UserName -p $Password -m
+
+echo "Create folder .vnc"
 # mkdir /home/test/.vnc
 mkdir /home/$UserName/.vnc
+
+echo "Create folder VNC Password file"
 # echo "123456" | vncpasswd -f > /home/test/.vnc/passwd
 echo $VNCPassword | vncpasswd -f > /home/$UserName/.vnc/passwd
+
+echo "Change owner of .vnc folder"
 # chown -R test:test /home/test/.vnc
 chown -R $UserName:$UserName /home/$UserName/.vnc
+
+echo "Change mod of .vnc/passwd file"
 # chmod 0600 /home/test/.vnc/passwd
 chmod 0600 /home/$UserName/.vnc/passwd
+
+echo "Switch to user $UserName"
 # su test
 su $UserName
+
+echo "Start VNC Server"
 vncserver
 
 

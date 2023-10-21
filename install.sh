d3d5e583b063368d777b362e7deb4c062eb7c9f4
ohainz <124523037+ohainz@users.noreply.github.com>
2023-10-21 22:12:58 +0200

    Reduce the output during docker build

--- install.sh
+++ install.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
 
-apt update && apt upgrade -y && apt install sudo locales --yes
+apt update -qq && apt upgrade -y -qq && apt install sudo locales --yes
 dpkg-reconfigure -f noninteractive tzdata
-apt update && apt upgrade -y
-apt install apt-utils -y
-apt update && apt upgrade -y
+apt update && apt upgrade -y -qq
+apt install apt-utils -y -qq
+apt update && apt upgrade -y -qq
 
 echo "--- install nano ---" 
-apt install nano -y
+apt install nano -y -qq 
 
 echo "--- install xmlstarlet ---" 
-apt install xmlstarlet -y
-
-echo "--- systemctl ---" 
-apt install systemctl -y
+apt install xmlstarlet -y -qq
 
 echo "--- install dbus-x11 ---" 
-apt install dbus-x11 -y
+apt install dbus-x11 -y -qq
 
 echo "--- install lighttpd ---" 
-apt install lighttpd -y
+apt install lighttpd -y -qq
 
 echo "--- install lxde-core ---" 
-apt install lxde-core -y
+apt install lxde-core -y -qq
 
 echo "--- install lxde ---" 
-apt install lxde -y
+apt install lxde -y -qq
 
 echo "--- install tightvncserver ---" 
-apt install tightvncserver -y
+apt install tightvncserver -y -qq
 
 echo "--- install chromium ---" 
-apt install chromium -y
+apt install chromium -y -qq
+
+echo "--- systemctl ---" 
+apt install systemctl -y -qq
 
 echo "--- Configure x11 ---" 
 echo "allowed_users = anybody" | tee -a /etc/X11/Xwrapper.config

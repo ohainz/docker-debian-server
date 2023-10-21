15e57db797cd25887e01dcdc471f96f2e1e56bf2
ohainz <124523037+ohainz@users.noreply.github.com>
2023-10-21 22:38:26 +0200

    Suppress warnings

--- install.sh
+++ install.sh
@@ -1,42 +1,43 @@
 #!/bin/bash
 
+export DEBIAN_FRONTEND=noninteractive
 apt -qq update && apt -qq upgrade -y && apt install sudo locales --yes
 dpkg-reconfigure -f noninteractive tzdata
 apt -qq update && apt -qq upgrade -y
-apt -qq install apt-utils -y
+apt -yq install apt-utils -y
 apt -qq update && apt -qq upgrade -y
 
 echo "--------------------------------------------------------------------------------" 
 echo "--- install nano ---" 
-apt -qq install nano -y 
+apt -yq install nano -y 
 
 echo "--------------------------------------------------------------------------------" 
 echo "--- install xmlstarlet ---" 
-apt -qq install xmlstarlet -y
+apt -yq install xmlstarlet -y
 
 echo "--------------------------------------------------------------------------------" 
 echo "--- install dbus-x11 ---" 
-apt -qq install dbus-x11 -y
+apt -yq install dbus-x11 -y
 
 echo "--------------------------------------------------------------------------------" 
 echo "--- install lighttpd ---" 
-apt -qq install lighttpd -y
+apt -yq install lighttpd -y
 
 echo "--------------------------------------------------------------------------------" 
 echo "--- install lxde ---" 
-DEBIAN_FRONTEND=noninteractive apt -yq install lxde -y
+apt -yq install lxde -y
 
 echo "--------------------------------------------------------------------------------" 
 echo "--- install tightvncserver ---" 
-apt -qq install tightvncserver -y
+apt -yq install tightvncserver -y
 
 echo "--------------------------------------------------------------------------------" 
 echo "--- install chromium ---" 
-apt -qq install chromium -y
+apt -yq install chromium -y
 
 echo "--------------------------------------------------------------------------------" 
 echo "--- systemctl ---" 
-apt -qq install systemctl -y
+apt -yq install systemctl -y
 
 echo "--------------------------------------------------------------------------------" 
 echo "--- Configure x11 ---" 
@@ -47,5 +48,6 @@ echo "--- Configure lighttpd ---"
 echo "dir-listing.activate = \"enable\"" | tee -a /etc/lighttpd/lighttpd.conf
 echo "dir-listing.encoding = \"utf-8\"" | tee -a /etc/lighttpd/lighttpd.conf
 
+export DEBIAN_FRONTEND=
 systemctl restart lighttpd
 
